b07ae77de81c88beef430b38f8e514de5334b912
healingdrawing <healingdrawing@gmail.com>
2023-12-22 06:08:29 +0200

    raw. need polish outputs

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/healingdrawing/penishell
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+
+	bang()
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("$ ")
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			// Handle EOF (Ctrl+D)
+			if err == io.EOF {
+				return
+			}
+			fmt.Println(err)
+			continue
+		}
+		command = strings.TrimSpace(command)
+		if command == "exit" {
+			break
+		}
+		args := strings.Split(command, " ")
+		switch args[0] {
+		case "cd":
+			var dir string
+			if len(args) > 1 {
+				dir = args[1]
+			} else {
+				homeDir, err := os.UserHomeDir()
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				dir = homeDir
+			}
+			err := os.Chdir(dir)
+			if err != nil {
+				fmt.Println(err)
+			}
+		default:
+			cmd := exec.Command(args[0], args[1:]...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+}
